09325b515c357704001e51f5ffc8bb16352511f9
Vjacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2023-06-06 14:20:38 +0300

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,18 @@ import { createRouter, createWebHistory, RouterView } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SingleCard from '../views/SingleCard.vue'
 import About from '../views/AboutView.vue'
-import Tr from '@/i18n/translation'
+// import Tr from '@/i18n/translation'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
-      path: '/vue_mars_site/:locale?',
+      path: '/vue_mars_site/',
       component: RouterView,
-      beforeEnter: Tr.routeMiddleware,
+      // beforeEnter: Tr.routeMiddleware,
       children: [
         {
-          path: '/vue_mars_site/:locale?',
+          path: '/vue_mars_site/',
           name: 'home',
           component: HomeView
         },
@@ -23,7 +23,7 @@ const router = createRouter({
           component: About
         },
         {
-          path: '/:locale?/images/:imageId',
+          path: '/:imageId',
           name: 'singleCard',
           component: SingleCard,
           props: true
