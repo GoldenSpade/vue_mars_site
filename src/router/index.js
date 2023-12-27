edd004a9fd2965e6ebfdba35c1dbd0a32cdaecea
Vjacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2023-12-27 20:05:00 +0200

    up

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory, RouterView } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SingleCard from '../views/SingleCard.vue'
-import About from '../views/AboutView.vue'
-// import Tr from '@/i18n/translation'
+import NoDataMessage from '../components/NoDataMessage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_BASE_URL),
@@ -10,17 +9,11 @@ const router = createRouter({
     {
       path: '/vue_mars_site/',
       component: RouterView,
-      // beforeEnter: Tr.routeMiddleware,
       children: [
         {
           path: '/vue_mars_site/',
           name: 'home',
           component: HomeView
-        },
-        {
-          path: '/vue_mars_site/about',
-          name: 'about',
-          component: About
         },
         {
           path: '/vue_mars_site/images/:imageId',
@@ -30,10 +23,10 @@ const router = createRouter({
         },
         {
           path: '/vue_mars_site/:notFound(.*)',
-          component: About
+          component: NoDataMessage
         }
       ]
-    },
+    }
   ]
 })
 
